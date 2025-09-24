73ec3f55323f951601aa6b2a1f74384ea77bd368
eeshah-S-khan <eeshahsabirkhan144@gmail.com>
2025-09-24 10:01:55 +0300

    2FA

---
+++ signup/mail.php
@@ -0,0 +1,31 @@
+<?php
+extract ($_POST);
+//Import PHPMailer classes into the global namespace
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader (created by composer, not included with PHPMailer)
+require 'vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'eeshahsabirkhan144@gmail.com';                     //SMTP username
+    $mail->Password   = 'mdkkdiifelpowe5k';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('eeshahsabirkhan144@gmail.com', 'Mailer');
+    $mail->addAddress('abc@example.net', 'abc User');     //Add a recipient
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+?>
