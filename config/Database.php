13095d76cdf33108a17ea19f201cbd8063b80e07
Moses <159237123+mmugo79@users.noreply.github.com>
2025-10-12 18:52:28 +0300

    commit 3

---
+++ config/Database.php
@@ -0,0 +1,28 @@
+<?php
+namespace Config;
+
+use PDO;
+use PDOException;
+
+class Database {
+    private $host = '127.0.0.1';
+    private $db_name = 'vehicle_pro';
+    private $username = 'root';
+    private $password = '';
+    public $conn;
+
+    public function getConnection(){
+        $this->conn = null;
+        try{
+            $dsn = "mysql:host={$this->host};dbname={$this->db_name};charset=utf8mb4";
+            $this->conn = new PDO($dsn, $this->username, $this->password, [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            ]);
+        }catch(PDOException $e){
+            echo "DB Connection error: " . $e->getMessage();
+            exit;
+        }
+        return $this->conn;
+    }
+}
+
